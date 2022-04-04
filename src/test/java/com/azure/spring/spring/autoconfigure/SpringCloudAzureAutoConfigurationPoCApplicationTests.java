8c0d3e934a72e3c84d628b20f1c688071b819346
Strong Liu <shaozliu@microsoft.com>
2022-04-04 22:37:25 +0800

    first commit

---
+++ src/test/java/com/azure/spring/spring/autoconfigure/SpringCloudAzureAutoConfigurationPoCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.spring.spring.autoconfigure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAzureAutoConfigurationPoCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
